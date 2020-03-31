4f3273cf53706be1a0475d10ff86bd24085f760c
Ari <ari@fullstack.io>
2020-03-31 10:13:53 -0400

    Updates with new theme

--- zsh/features/04_prompt.zsh
+++ zsh/features/04_prompt.zsh
@@ -1,4 +1,7 @@
-# prompt powerlevel9k
-autoload -Uz promptinit
-eval "$(starship init zsh)"
+# autoload -Uz promptinit
+# promptinit
+# prompt p10k
+source /usr/local/opt/powerlevel10k/powerlevel10k.zsh-theme
+[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+# eval "$(starship init zsh)"
 # export PROMPT="$PROMPT\$(git-radar --zsh --fetch) "

b0f41fa041d69dfabb239087f9e26ba9ac67b0b1
Ari <ari@fullstack.io>
2020-05-20 10:46:39 -0400

    WIP

--- zsh/features/06_android.zsh
+++ zsh/features/06_android.zsh
@@ -1,2 +1,11 @@
 export INTEL_HAXM_HOME=/usr/local/Caskroom/intel-haxm
 export JAVA_HOME=$(/usr/libexec/java_home -v 1.8)
+
+export ANDROID_HOME=~/Library/Android/sdk
+export ANDROID_SDK_ROOT=~/Library/Android/sdk
+export ANDROID_AVD_HOME=~/.android/avd
+
+export PATH=$ANDROID_SDK_ROOT/tools:$PATH
+export PATH=$ANDROID_SDK_ROOT/tools/bin:$PATH
+export PATH=$ANDROID_SDK_ROOT/platform-tools:$PATH
+export PATH=$ANDROID_SDK_ROOT/emulator:$PATH

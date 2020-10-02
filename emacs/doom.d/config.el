6102e819c083ee80e96114177ef71d106be9bda0
Ari Lerner <alerner@amazon.com>
2020-10-02 16:07:05 -0400

    WIP

--- emacs/doom.d/config.el
+++ emacs/doom.d/config.el
@@ -81,6 +81,24 @@
  :n "M-j" #'org-metaup
  :n "M-k" #'org-metadown)
 
+(add-hook 'js2-mode-hook 'prettier-js-mode)
+(add-hook 'web-mode-hook 'prettier-js-mode)
+
+(defun setup-tide-mode ()
+  (interactive)
+  (tide-setup)
+  (flycheck-mode +1)
+  (setq flycheck-check-syntax-automatically '(save mode-enabled))
+  (eldoc-mode +1)
+  (prettier-js-mode +1)
+  (tide-hl-identifier-mode +1)
+  (company-mode +1))
+
+(setq company-tooltip-align-annotations t)
+
+(add-hook 'before-save-hook 'tide-format-before-save)
+(add-hook 'typescript-mode-hook #'setup-tide-mode)
+
 (setq
  doom-font (font-spec :family "SF Mono")
  js-indent-level 2
--- zsh/features/10_emacs.zsh
+++ zsh/features/10_emacs.zsh
@@ -1,4 +1,4 @@
-export PATH=/usr/local/opt/emacs-plus@28/bin:$PATH
+export PATH=/usr/local/opt/emacs-plus/bin:$PATH
 alias emax='
 export DISPLAY=:0.0
 export LIBGL_ALWAYS_INDIRECT=1

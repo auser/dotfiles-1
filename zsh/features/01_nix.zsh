31514c886fedcaecf7122a52f9513757ce6cd5d1
Ari Lerner <me@ari.io>
2021-02-11 15:23:55 -0500

    WIP

--- zsh/features/01_nix.zsh
+++ zsh/features/01_nix.zsh
@@ -3,3 +3,5 @@ if [ -f /Users/alerner/.nix-profile/etc/profile.d/nix.sh ]; then
 fi
 
 export NIX_PATH=darwin-config=$HOME/.nixpkgs/darwin-configuration.nix:$HOME/.nix-defexpr/channels:$NIX_PATH
+
+#figlet roar | cowsay -n dragon | lolcat
---
+++ zsh/features/terminal.zsh
@@ -0,0 +1,2 @@
+echo "HELLO WORLD"
+figlet TERMINAL | cowsay -n | lolcat
